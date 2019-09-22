e1645c44b7696790f1ed9aeb6009f215c12369e4
Thomas Boby <th.boby@gmail.com>
2019-09-22 19:52:07 +0100

    Use local tools

--- build.cmd
+++ build.cmd
@@ -1,23 +1,10 @@
 @echo off
 cls
 
-SET BUILD_PACKAGES=BuildPackages
-
-IF NOT EXIST "%BUILD_PACKAGES%\fake.exe" (
-  dotnet tool install fake-cli ^
-    --tool-path ./%BUILD_PACKAGES% ^
-    --version 5.*
-)
-
-REM Un-Rem these after changing build scripts
-REM IF EXIST ".fake"          (RMDIR /Q /S ".fake"         )
-REM IF EXIST "build.fsx.lock" (DEL         "build.fsx.lock")
-
-"%BUILD_PACKAGES%/fake.exe" run build.fsx --target %*
-
-REM .paket\paket.exe restore
+dotnet tool restore
+dotnet paket restore
 if errorlevel 1 (
   exit /b %errorlevel%
 )
 
-REM packages\build\FAKE\tools\FAKE.exe build.fsx %* --nocache
+dotnet fake run build.fsx --target %*
