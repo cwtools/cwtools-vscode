b1dd9a5f835d78dc35ad3616f85b5579adb28f27
tboby <th.boby@gmail.com>
2025-07-11 07:59:38 +0100

    Update more packages

--- client/test/suite/index.ts
+++ client/test/suite/index.ts
@@ -1,8 +1,8 @@
 import * as path from 'path';
 import Mocha from 'mocha';
-import glob from 'glob';
+import { glob } from 'glob';
 
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
     // Create the mocha test
     const mocha = new Mocha({
         ui: 'tdd',
@@ -11,15 +11,13 @@ export function run(): Promise<void> {
 
     const testsRoot = path.resolve(__dirname, '..');
 
-    return new Promise((c, e) => {
-        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-            if (err) {
-                return e(err);
-            }
+    try {
+        const files = await glob('**/**.test.js', { cwd: testsRoot });
+        
+        // Add files to the test suite
+        files.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
 
-            // Add files to the test suite
-            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
+        return new Promise((c, e) => {
             try {
                 // Run the mocha test
                 mocha.run(failures => {
@@ -33,5 +31,7 @@ export function run(): Promise<void> {
                 e(err);
             }
         });
-    });
+    } catch (err) {
+        throw err;
+    }
 }
