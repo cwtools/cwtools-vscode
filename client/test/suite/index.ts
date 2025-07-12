bbd74c631194276e2072a74112c0eef7a943544c
tboby <th.boby@gmail.com>
2025-07-12 13:34:06 +0100

    Vaguely working test setup

--- client/test/suite/index.ts
+++ client/test/suite/index.ts
@@ -13,9 +13,9 @@ export async function run(): Promise<void> {
 
     try {
         const files = await glob('**/**.test.js', { cwd: testsRoot });
-        
+
         // Add files to the test suite
-        files.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
+        files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
         return new Promise((c, e) => {
             try {
