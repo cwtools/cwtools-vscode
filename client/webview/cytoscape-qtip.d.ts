077c85708da8d55c255a95f8a8c3241e31cf4b0e
tboby <th.boby@gmail.com>
2025-07-29 07:29:34 +0100

    Vaguely working updated tippy/popper

--- client/webview/cytoscape-qtip.d.ts
+++ client/webview/cytoscape-qtip.d.ts
@@ -3,6 +3,5 @@ declare module "cytoscape-dagre";
 declare module "cytoscape-navigator";
 declare module "cytoscape-canvas";
 declare module "cytoscape-elk";
-declare module "cytoscape-popper";
 declare module "merge-images";
 declare module "tippy.js/esm/index";
