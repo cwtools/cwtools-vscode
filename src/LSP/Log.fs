07caf3069f8bfe30ac75495aca51f3175ff7e7d6
tboby <th.boby@gmail.com>
2025-08-08 07:57:09 +0100

    Cell value

--- src/LSP/Log.fs
+++ src/LSP/Log.fs
@@ -3,4 +3,5 @@ module LSP.Log
 let diagnosticsLog = ref stderr
 
 /// Print to LSP.Log.diagnosticsLog, which is stderr by default but can be redirected
-let dprintfn (fmt: Printf.TextWriterFormat<'T>) : 'T = Printf.fprintfn !diagnosticsLog fmt
+let dprintfn (fmt: Printf.TextWriterFormat<'T>) : 'T =
+    Printf.fprintfn diagnosticsLog.Value fmt
