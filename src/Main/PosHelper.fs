ad1bb446d41aa0694206a0963c9e1b9efd5c0e66
textGamex <textGamex@outlook.com>
2025-09-24 01:30:43 +0800

    Line assistant method move to cwtool-vscode

--- src/Main/PosHelper.fs
+++ src/Main/PosHelper.fs
@@ -2,6 +2,13 @@
 
 open CWTools.Utilities.Position
 
+module Line =
+    // Visual Studio uses line counts starting at 0, F# uses them starting at 1
+    let fromZ (line: Line0) = int line + 1
+
+    let toZ (line: int) : Line0 =
+        LanguagePrimitives.Int32WithMeasure(line - 1)
+
 module PosHelper =
-   let fromZ (line: Line0) idx = mkPos (Line.fromZ line) idx
-   let toZ (p: pos) = (Line.toZ p.Line, p.Column)
+    let fromZ (line: Line0) idx = mkPos (Line.fromZ line) idx
+    let toZ (p: pos) = (Line.toZ p.Line, p.Column)
